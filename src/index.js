b0f65f2604a501842268343928df581367143b2a
Bruno Patrício <bruno.patricio3@hotmail.com>
2022-01-22 02:11:02 +0000

    Started implementation of landing page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Application from './pages/Application';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/subpages/Home/index.js
+++ src/pages/subpages/Home/index.js
@@ -3,8 +3,24 @@ import '../../../styles/main.scss';
 function Home() {
   return (
     <>
-      <div className="">
-        Hello World!
+      <div className="container margin-bottom-m">
+        <div className="row nav">
+          <div className="col-sm">
+            <a className='nav-item'>
+              Website
+            </a> 
+          </div>
+        </div>
+      </div>
+       <div class="container">
+        <div class="row ">
+          <div class="col-sm text-center">
+            One of two columns
+          </div>
+          <div class="col-sm text-center">
+            One of two columns
+          </div>
+        </div>
       </div>
     </>
   );
