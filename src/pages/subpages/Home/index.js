1c4cb9477a1325a5b45d3f19859a1dfd84bd8448
Bruno Patrício <bruno.patricio3@hotmail.com>
2022-01-24 19:12:43 +0000

    adding some colors and changing some logic

--- src/pages/subpages/Home/index.js
+++ src/pages/subpages/Home/index.js
@@ -3,7 +3,7 @@ import '../../../styles/main.scss';
 function Home() {
   return (
     <>
-      <div className="container margin-bottom-m">
+      <div className="container">
         <div className="row nav">
           <div className="col-sm">
             <a className='nav-item'> Website Link </a> 
@@ -36,9 +36,13 @@ function Home() {
       </div>
 
       <div className="container technologies margin-bottom-m">
-        <div className="row technologies-list">
-          <div className="col-sm">
+      <div className="row">
+          <div class="col-sm">
             <h2>Technologies</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
           </div>
           <div className="col-sm">
@@ -86,7 +90,9 @@ function Home() {
             </div>
           </div>
         </div>
+        <div className="fixed-menu"></div>
       </div>
+
     </>
   );
 }
