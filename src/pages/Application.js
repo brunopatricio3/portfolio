439a02c352ff6de364fdc6f591679859c86e5f5c
Bruno Patrício <bruno.patricio3@hotmail.com>
2022-02-06 00:22:57 +0000

    Started over portfolio with new idea

--- src/pages/Application.js
+++ src/pages/Application.js
@@ -2,15 +2,12 @@ import React, { useState } from 'react';
 import Home from './subpages/Home'
 import '../styles/main.scss';
 
-
 function Application() {
   const [selectedPage, setSelectedPage] = useState("Home");
 
   return (
     <div className="app-wrapper">
-      <div className="main-wrapper">
-          <Home who="Bruno"/> 
-      </div>
+          <Home /> 
     </div>
   );
 }
--- src/pages/subpages/Home/index.js
+++ src/pages/subpages/Home/index.js
@@ -1,34 +1,17 @@
+import React, { useState } from 'react';
 import '../../../styles/main.scss';
+import AnimatedBackground from "../../../assets/videos/animated-background.mp4"
 
 function Home() {
+
+  const [expandProfile, setExpandProfile] = useState(false);
+
   return (
     <>     
-      <div className="container transition margin-bottom-m">
-        <div className="row">
-          <div className="col-sm text-center ">
-            <h4>Hello there!</h4>
-            <h2>I'm <span className="txt-cl-orange">Bruno Patrício</span>,</h2>
-            <h5>A Front-End developer.</h5>  
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col-sm  text-center ">
-            <span className="isPressable margin-x-sm" onClick={() => alert("Download CV")}>
-              <span className="text-sz-lg txt-cl-prog-grey">&#60;</span>
-              <span className="text-sz-lg txt-cl-prog-green">Resume</span>
-              <span className="text-sz-lg txt-cl-prog-grey">/&#62;</span> 
-            </span>
-            <span className="isPressable" onClick={() => alert("About me")}>
-              <span className="text-sz-lg txt-cl-prog-grey">&#60;</span>
-              <span className="text-sz-lg txt-cl-prog-green isPressable">Aboutme</span>
-              <span className="text-sz-lg txt-cl-prog-blue"> who</span>
-              <span className="text-sz-lg txt-cl-prog-white">=</span>
-              <span className="text-sz-lg txt-cl-prog-brown">"Bruno"</span>
-              <span className="text-sz-lg txt-cl-prog-grey">/&#62;</span> 
-            </span>
-          </div>
-        </div>
+      <div className="main-wrapper">
+        <video className="bg-video" autoPlay loop muted>
+            <source src={AnimatedBackground} type='video/mp4' />
+        </video>
       </div>    
     </>
   );
