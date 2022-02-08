1f06f0c7a9ac09906bf750668078342a97f71dc1
Bruno Patrício <bruno.patricio3@hotmail.com>
2022-02-08 12:25:26 +0000

    build for github pages

--- src/pages/subpages/Home/SectionWelcome.js
+++ src/pages/subpages/Home/SectionWelcome.js
@@ -13,18 +13,18 @@ function SectionWelcome() {
         <div className="bg-video-container">
           <div className="bg-video-text-container">
           <h1 className="text-sz-xxl">
-              <span className='txt-cl-prog-grey'>&#60;</span>
-              <span className='txt-cl-prog-green'>BrunoPatricio</span> 
-              <span className='txt-cl-prog-blue'> role</span>
-              <span className='txt-cl-prog-white'>=</span>
-              <span className='txt-cl-prog-brown'>"FrontEndDeveloper"</span>
-              <span className='txt-cl-prog-grey'> /</span>
-              <span className='txt-cl-prog-grey'>&#62;</span>
-            </h1>
+            <span className='txt-cl-prog-grey'>&#60;</span>
+            <span className='txt-cl-prog-green'>BrunoPatricio</span> 
+            <span className='txt-cl-prog-blue'> role</span>
+            <span className='txt-cl-prog-white'>=</span>
+            <span className='txt-cl-prog-brown'>"FrontEndDeveloper"</span>
+            <span className='txt-cl-prog-grey'> /</span>
+            <span className='txt-cl-prog-grey'>&#62;</span>
+          </h1>
             
             <SocialIcon icon={"devicon-github-original"} link="github" />
             <SocialIcon icon={"devicon-linkedin-plain"} link="linkedin" />
-                        
+
             {/* <p className="text-sz-xl">Front-End Developer</p> */}
           </div>
         </div>
